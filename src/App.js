2a7fda5573d1d62ed07cb37697bb32c458e28721
Jesus Martinez <jesmartinez@bgeneral.com>
2022-08-10 18:27:00 -0500

    upload project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './terra.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jesus Martinez Test
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Easy + terraform
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
